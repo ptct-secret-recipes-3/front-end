705740f065ecc62108e4f2170dc32ec5c9ca0e15
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-07-26 15:13:54 -0400

    remove imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// import RegisterForm from './components/Register'
-// import LoginForm from './components/Login'
-
 
 function App() {
   return (
